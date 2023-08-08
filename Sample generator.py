f18a76d2e0770efc9b34c284f82301696b4d0a4e
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2023-08-09 00:25:03 +0530

    Adding succesful file sfor the first time

---
+++ Sample generator.py
@@ -0,0 +1,40 @@
+import cv2
+
+cam = cv2.VideoCapture(0)
+cam.set(3, 640)
+cam.set(4, 840)
+
+detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+face_id = input("Enter a Numeric USER ID here :   ")
+
+print("Taking Samples please Look at the Camera......!")
+count = 0
+
+while True:
+
+    ret, img = cam.read()
+    converted_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = detector.detectMultiScale(converted_img, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        count += 1
+
+        cv2.imwrite("samples/face." + str(face_id)+'.' +
+                    str(count)+".jpg", converted_img[y:y+h, x:x+w])
+
+        cv2.imshow('image', img)
+
+    k = cv2.waitKey(100) & 0xff
+
+    if k == 27:
+        break
+    elif count >= 10:
+        print("\nDone! Time to Exit")
+        break
+
+print("Samples Taken Now closing the program....")
+cam.release()
+cv2.destroyAllWindows()
