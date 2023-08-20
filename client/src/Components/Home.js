89063bbddf412307ae04bc30af7357f73fe5776e
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2023-08-20 18:30:04 +0530

    Adding the client files

---
+++ client/src/Components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <>
+            {/* <p><b>This is Home</b></p> */}
+            <h1 className='text-3xl p-4'>This is Home</h1>
+        </>
+    )
+}
