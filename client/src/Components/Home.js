28d693572245fa8824cc1c26d1110ba485edfeaa
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2023-08-21 18:28:28 +0530

    Adding the gitignore files

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -4,7 +4,13 @@ export default function Home() {
     return (
         <>
             {/* <p><b>This is Home</b></p> */}
-            <h1 className='text-3xl p-4'>This is Home</h1>
+            <div className="container mx-auto flex justify-center items-center h-[100vh] flex-col bg-slate-400">
+                <div className="bg-slate-900 h-[50vh] flex justify-center items-center flex-col rounded-2xl">
+                    <h1 className='text-3xl p-3 text-[#0095ff]'>Automated Student Attendence...👨🏻‍🎓</h1>
+                    <button onClick={() => alert('Attendence is registerd')} className='p-3 text-[#ff7070]'>Present</button>
+                    <button onClick={() => alert('Attendence is Not-Registerd')} className='p-3 text-[#ff7070]'>Absent</button>
+                </div>
+            </div>
         </>
     )
 }
